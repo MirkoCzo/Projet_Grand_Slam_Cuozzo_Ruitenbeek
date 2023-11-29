acfaa910d224f4a2210ec3e7670ae4194eeb396a
Expensivenn <cedric.ruitenbeek@condorcet.be>
2023-11-29 16:19:30 +0100

    SetDao

---
+++ Model/Games.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projet_Grand_Slam_Cuozzo_Ruitenbeek.Model
+{
+    internal class Games
+    {
+    }
+}
